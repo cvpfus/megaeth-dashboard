5f6de4c0247147b55727b51af813c5ba104474be
cvpfus <47532266+cvpfus@users.noreply.github.com>
2025-11-02 23:48:58 +0700

    feat: add simple analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,40 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
-const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+const config = defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+      {
+        name: "simpleanalytics",
+        transformIndexHtml(html) {
+          const file = mode === "development" ? "latest.dev.js" : "latest.js";
+          return {
+            html,
+            tags: [
+              {
+                tag: "script",
+                attrs: {
+                  async: true,
+                  src: `https://scripts.simpleanalyticscdn.com/${file}`,
+                },
+                injectTo: "head",
+              },
+            ],
+          };
+        },
+      },
+    ],
+  };
+});
 
-export default config
+export default config;
